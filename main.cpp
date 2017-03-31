d83ad1a7ef7f67bdc56a23674056aaed1860be0e
TheKipmaster <fkipman@hotmail.com>
2017-03-31 20:28:02 -0300

    -.gitignore criado -Executável adicionado ao .gitignore -Esboço de menu

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main () {
+
+  cout << "\tOpa, tudo bom? Bem vindo à biblioteca do Vaticano" << endl << endl;
+  cout << "1 - Fazer doação" << endl;
+  cout << "2 - Tuor da biblioteca" << endl;
+  cout << "3 - Você sabe quantos livros nós temos?" << endl;
+  cout << "4 - *Roubar livro*" << endl;
+}
