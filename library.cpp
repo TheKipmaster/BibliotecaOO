ac78191c135f4c5792b70fabadb964ae2e355132
Danilo de Lima <danilodelyima@gmail.com>
2017-04-20 17:24:34 -0300

    -Bug semi-selecionado

--- library.cpp
+++ library.cpp
@@ -8,18 +8,18 @@ Library::Library(void) {}
 
 void Library::addBook(Book book) {
 	book.setId(0);
-	push_back(book);
+	library.push_back(book);
 }
 
 void Library::removeBook(int id) {
-	Book book = at(id);
-	erase(id);
+	Book book = library.at(id);
+	library.erase(id);
 	std::cout << book.getName() << "removido com sucesso" << std::endl;
 }
 
 void Library::list(void) {
 	int i;
-	for(i=0; i<size(); i++) {
-		std::cout << "id: " << i << " nome: " << at(i).getName() << std::endl;
+	for(i=0; i<library.size(); i++) {
+		std::cout << "id: " << i << " nome: " << library.at(i).getName() << std::endl;
 	}
 }
