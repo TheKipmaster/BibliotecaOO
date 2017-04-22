fd8b928a427cd22229d67686796e59722c2af598
TheKipmaster <fkipman@hotmail.com>
2017-04-22 09:38:30 -0300

    -Adiciona um espaço na saída da função removeBook da biblioteca

--- library.cpp
+++ library.cpp
@@ -11,15 +11,15 @@ void Library::addBook(Book book, int* size) {
 void Library::removeBook(int id) {
 	Book book = collection.at(id);
 	collection.erase(collection.begin()+id);
-	std::cout << book.getName() << "removido com sucesso" << std::endl;
+	std::cout << book.getName() << " removido com sucesso" << std::endl;
 }
 
 void Library::list(void) {
 	unsigned i;
 	for(i=0; i<collection.size(); i++) {
-		std::cout << "id: " << collection[i].getId() 
-				  << " nome: " << collection.at(i).getName() 
-				  << " Author: "<< collection.at(i).getAuthor() 
+		std::cout << "id: " << collection[i].getId()
+				  << " nome: " << collection.at(i).getName()
+				  << " Author: "<< collection.at(i).getAuthor()
 				  << std::endl;
 	}
 }
